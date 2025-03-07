236390aaf10ece2fe5e95f9fa36e52c60787b605
deniscelant <dedecelant123@gmail.com>
2025-03-07 16:38:32 -0300

    alpha

--- script.js
+++ script.js
@@ -1,2 +1,52 @@
-const images = [];
-let active = images[0];
+let images = ["red", "blue", "white", "yellow", "green"];
+let active = images[2];
+
+// const last = images.pop()
+// images.unshift(last)
+
+// console.log(last)
+// console.log(images)
+
+let index = 0;
+
+const card = document.getElementsByClassName("card");
+const arrowLeft = document.getElementById("arrowLeft");
+const arrowRight = document.getElementById("arrowRight");
+
+Array.from(card).forEach((c) => {
+  c.style.backgroundColor = images[index++];
+
+});
+arrowLeft.onclick = () => {
+    if(index >= 5){
+        index = 0;
+    }
+  const last = images.pop();
+  images.unshift(last);
+
+  Array.from(card).forEach((c) => {
+    c.style.backgroundColor = images[index++];
+  
+  });
+  // Array.from(card).forEach(() => {
+  //   c.style.backgroundColor = images[index++];
+  // });
+  console.log(images);
+};
+
+arrowRight.onclick = () => {
+    if(index >= 5){
+        index = 0;
+    }
+  const first = images.shift();
+  images.push(first);
+
+  Array.from(card).forEach((c) => {
+    c.style.backgroundColor = images[index++];
+  
+  });
+  // Array.from(card).forEach(() => {
+  //   c.style.backgroundColor = images[index++];
+  // });
+  console.log(images);
+};
